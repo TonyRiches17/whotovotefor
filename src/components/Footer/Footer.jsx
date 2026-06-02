cb3b6b13a73b474770d0ebb31163c22533b76fab
Rich <TonyRiches17@yahoo.com>
2026-06-02 19:34:13 -0400

    Add Components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return(
+    <footer className="footer">
+      <p className="footer__copyright">&copy; Ri¢h 2026</p>
+    </footer>
+  )
+}
+
+export default Footer;
